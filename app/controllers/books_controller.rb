5747d962f7c0f4229eb7046f5d921bcc8221fb3b
EC2 Default User <ec2-user@ip-172-31-0-208.ap-northeast-1.compute.internal>
2024-08-30 23:35:08 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,55 @@
 class BooksController < ApplicationController
+  def index
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new  
+    @books = Book.all  
+  end
+  
   def new
   end
+  
+  def create
+  @book = Book.new(book_params)
+  if @book.save
+    flash[:notice] = 'successfully'
+    redirect_to book_path(@book)  # show アクションにリダイレクト
+  else
+    @books = Book.all
+    flash.now[:alert] = 'error'
+    render :index
+  end
+  end
 
-  def index
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = 'successfully。'
+    redirect_to books_path
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'successfully'
+      redirect_to book_path(@book)  # show アクションにリダイレクト
+    else
+      flash[:alert] = 'error'
+      render :edit
+    end
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#new'
-  get '/index' => 'books#index'
-  get '/show' => 'books#show'
-  get '/edit' => 'books#edit'
   get '/' => 'homes#top'
-  post 'books' => 'books#create'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
